6567d4d7ce5fdc632716e1bfb6cc592097ddd2a5
Fernando Mercadal <fermercadal@outlook.com>
2021-10-31 08:16:45 -0300

    Add Cart Slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import cartSlice from "./cart-slice";
 import uiSlice from "./ui-slice";
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer },
+  reducer: { cart: cartSlice.reducer, ui: uiSlice.reducer },
 });
 
 export default store;
